034fb39c8e5ae09d5278aa663c76240d1ffb7e02
acek1216 <ace.k1216@gmail.com>
2025-08-16 01:18:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 import threading
 from bot import run_bot
 
@@ -7,9 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Bot is running"
+    return "Bot is running!"
 
 if __name__ == "__main__":
+    # Discord Bot を別スレッドで起動
     threading.Thread(target=run_bot).start()
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+
+    # Flask をPORT=8080で起動（Cloud Run 要件）
+    app.run(host="0.0.0.0", port=8080)
